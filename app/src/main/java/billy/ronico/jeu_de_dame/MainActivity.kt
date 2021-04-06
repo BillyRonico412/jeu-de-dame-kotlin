b22fe03756d00eb6e4080525fbae9c891007625b
BillyRonico <billyronico412@gmail.com>
2021-04-06 17:45:31 +0400

    Premier commit, Projet vierge

---
+++ app/src/main/java/billy/ronico/jeu_de_dame/MainActivity.kt
@@ -0,0 +1,11 @@
+package billy.ronico.jeu_de_dame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
